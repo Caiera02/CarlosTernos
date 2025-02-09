a81c59358f59378043d6f230e9cb4daadcc67311
Caiera02 <caiocezar2807@gmail.com>
2025-02-08 21:43:50 -0300

    Add o model tenro = Adulto

--- produtos/models.py
+++ produtos/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Terno (models.Model):
-    descricao: models.CharField(max_length=60,verbose_name = 'Texto')
-    img = models.ImageField(upload_to='produtos',verbose_name='Imagem 1')
+    titulo = models.CharField(max_length=20,verbose_name= 'Titulo')
+    descricao= models.TextField(max_length=100,verbose_name = 'Texto')
+    img = models.ImageField(upload_to='produtos/',verbose_name='Imagem 1')

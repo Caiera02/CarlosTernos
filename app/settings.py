c2dffd6b87e236cea5ee852c9869466f364fac08
Caiera02 <caiocezar2807@gmail.com>
2025-02-13 12:58:58 -0300

    rodeio o comando colectsatic

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8er3!#0$6%qqfk)866(*3kk5egy+e7c0i0t1@sfqwd1jaux==3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition

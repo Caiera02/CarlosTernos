4c70d26b9b330b9f1aadfbf69da3a6f944e938d2
Caiera02 <caiocezar2807@gmail.com>
2025-04-04 16:02:50 -0300

    add mais coisa do admin

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
-from .models import Terno, Infantil, Tamanho
+from .models import Tamanho,Terno, Infantil
 # Register your models here.
-
 
 @admin.register(Tamanho)
 class TamanhoAdmin(admin.ModelAdmin):
     list_display = ('numero',)
+    ordering = ('numero',)
+    
     
 @admin.register(Terno)
 class TernoAdmin(admin.ModelAdmin):
-    list_display = ('titulo','descricao',)
-    # search_fields = ('descricao',)
+    list_display = ('titulo','descricao','mostrar_tamanhos',)
+    list_filter = ('tamanho',)
+    search_fields = ('titulo', 'descricao')
+    list_display_links = ('titulo',)  # Só o título será clicável para edição
+    
+    def mostrar_tamanhos(self, obj):
+        return " - ".join([str(t) for t in obj.tamanho.all()])
+    mostrar_tamanhos.short_description = 'Tamanhos'
 
 @admin.register(Infantil)
 class InfantilAdmin(admin.ModelAdmin):

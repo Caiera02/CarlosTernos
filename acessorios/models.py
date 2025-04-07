a0d83bf0c40985ea421d7f68a61a5bd08fcb9561
Caiera02 <caiocezar2807@gmail.com>
2025-04-07 17:25:03 -0300

    começando a montar a pag acessorio.html

--- acessorios/models.py
+++ acessorios/models.py
@@ -6,4 +6,5 @@ class Acessorios(models.Model):
     nome= models.CharField(max_length=20)
     descricao=models.CharField(max_length=50)
     cor = models.CharField(max_length=10)
+    img = models.ImageField( upload_to='acessorios/',verbose_name='Imagem 1')
     
--- acessorios/views.py
+++ acessorios/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from acessorios.models import Acessorios
 
 # Create your views here.
 
 def acessorios_view(request):
+    acessorios = Acessorios.objects.all()
     return render(
         request,
         'Acessorio.html',
+        {'acessorio' : acessorios}
     )

fa05c8168237f96532fe894da3d701deeee29fa0
Caiera02 <caiocezar2807@gmail.com>
2025-06-09 17:01:27 -0300

    ajuste no signal

--- produtos/signals.py
+++ produtos/signals.py
@@ -1,7 +1,6 @@
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from produtos.models import Sapato
-from googleai_api.client import gerar_descricao
 
 @receiver(pre_save, sender=Sapato)
 def sapato_pre_save(sender,instance, **kwargs):
